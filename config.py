e02e3f1d78e1951a5d1e184114ddcdaf4b8cff66
jharms17 <43645269+jharms17@users.noreply.github.com>
2023-11-08 12:03:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,15 @@ aws_access_key_id = AKIAQYLPMN5HMKPRSA2C
 aws_secret_access_key = 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
 output = json
 region = us-east-2
+
+config = {
+    "mysql": {
+        "user": "root",
+        "pass": "secret",
+        "tables": {
+            "users": "tb_users"
+        }
+        # etc
+    }
+}
+
